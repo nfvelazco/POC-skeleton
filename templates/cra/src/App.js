9335ce4addcf1253f0f4d41412380f629fd49d30
Nicolas Velazco <nfvelazco@gmail.com>
2020-02-21 11:08:05 -0300

    Custom template cra

--- templates/cra/src/App.js
+++ templates/cra/src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          POC Skeleton app React para Snoop
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.snoopconsulting.com"
           target="_blank"
           rel="noopener noreferrer"
         >
