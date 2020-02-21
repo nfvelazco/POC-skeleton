27d22df561ad2843c580771ba2eb1c7070c16131
Nicolas Velazco <nfvelazco@gmail.com>
2020-02-21 10:07:46 -0300

    Agregue template de CRA

--- app.js
+++ app.js
@@ -1,28 +1,67 @@
+#!/usr/bin/env node
 const fs = require('fs');
 
-const createDir = (dirPath) => {
-    fs.mkdirSync(dirPath,{recursive:true},(error)=>{
-        if(error){
-            console.error('Creando carpeta Error: ', error);
-        }else{
-            console.log(`El directorio: ${dirPath}. Se creó con exito!`)
-        }
-    });
-}
 
-const createFile = (filePath,fileContent) => {
-    // fs.writeFileSync()
-    fs.appendFile(filePath,fileContent,(error)=>{
-        if(error){
-            console.error('Creando archivo Error: ', error);
-        }else{
-            console.log(`El archivo: ${filePath}. Se creó con exito!`)
-        }
-    });
-}
+const NEW_PATH = `${process.cwd()}/${process.argv.slice(2)[0]}/`;
+const TEMPLATE_PATH = `./templates/cra/`;
 
-const PATH = `${process.cwd()}/themp/carpeta/`;
-const CONTENT = 'Contenido uno';
+var mkdir = function(dir) {
+	// making directory without exception if exists
+	try {
+		fs.mkdirSync(dir, 0755);
+	} catch(e) {
+        rmdir(NEW_PATH);
+		if(e.code != "EEXIST") {
+            
+			throw e;
+		}
+	}
+};
 
-createDir(PATH);
-createFile(`${PATH}${process.argv.slice(2)[0]}`,CONTENT);
+var rmdir = function(dir) {
+	if (path.existsSync(dir)) {
+		var list = fs.readdirSync(dir);
+		for(var i = 0; i < list.length; i++) {
+			var filename = path.join(dir, list[i]);
+			var stat = fs.statSync(filename);
+			
+			if(filename == "." || filename == "..") {
+				// pass these files
+			} else if(stat.isDirectory()) {
+				// rmdir recursively
+				rmdir(filename);
+			} else {
+				// rm fiilename
+				fs.unlinkSync(filename);
+			}
+		}
+		fs.rmdirSync(dir);
+	} else {
+		console.warn("warn: " + dir + " not exists");
+	}
+};
+
+var copyDir = function(src, dest) {
+	mkdir(dest);
+	var files = fs.readdirSync(src);
+	for(var i = 0; i < files.length; i++) {
+		var current = fs.lstatSync(path.join(src, files[i]));
+		if(current.isDirectory()) {
+			copyDir(path.join(src, files[i]), path.join(dest, files[i]));
+		} else if(current.isSymbolicLink()) {
+			var symlink = fs.readlinkSync(path.join(src, files[i]));
+			fs.symlinkSync(symlink, path.join(dest, files[i]));
+		} else {
+			copy(path.join(src, files[i]), path.join(dest, files[i]));
+		}
+	}
+};
+
+var copy = function(src, dest) {
+    console.log(`* Copiando ${src} a ${dest}-------`);
+	var oldFile = fs.createReadStream(src);
+	var newFile = fs.createWriteStream(dest);
+	util.pump(oldFile, newFile);
+};
+
+copyDir(TEMPLATE_PATH,NEW_PATH);
